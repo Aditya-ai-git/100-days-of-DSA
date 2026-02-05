d25501150363043b1bc9067957e47fb5a14e4522
Aditya-ai-git <adityamewar353@gmail.com>
2026-02-05 22:20:45 +0530

    DSA

---
+++ Day1.c
@@ -0,0 +1,50 @@
+/*Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (the array)
+- Third line: integer pos (1-based position)
+- Fourth line: integer x (element to insert)
+
+Output:
+- Print the updated array (n+1 integers) in a single line, space-separated
+
+Example:
+Input:
+5
+1 2 4 5 6
+3
+3
+
+Output:
+1 2 3 4 5 6
+
+*/
+#include <stdio.h>
+int main()
+{
+    int n,i,pos,x;
+    printf("Enter the length of an array ");
+    scanf("%d",&n);
+    int arr[n+1];
+    printf("Enter the elements of array\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the position you want put the integer ");
+    scanf("%d",&pos);
+    printf("Enter the integer ");
+    scanf("%d",&x);
+    for(i=n;i>pos-1;i--)
+    {
+        arr[i]=arr[i-1];
+    }
+    arr[pos-1]=x;
+    printf("Output:\n");
+    for(i=0;i<=n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
